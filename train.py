25389e1b4f62e6e2e0e228483518e46a7e5ce7de
Sizz Flair <sizzflair97@gmail.com>
2025-01-17 02:13:50 +0900

    250117

--- train.py
+++ train.py
@@ -6,16 +6,24 @@ from utils.coding import encode_data
 from jaxtyping import UInt8
 from torchvision.datasets import MNIST
 from torch.utils.data import Dataset, DataLoader
+from tqdm.auto import tqdm
+import wandb
+
+wandb.init(project="nessler2009")
 
 num_steps = 50
 population = 2
+num_epochs = 100
 batch_size = 32
 num_workers = 4
 out_features = 10
+learning_rate = 1e-1
+device = th.device("cuda" if th.cuda.is_available() else "cpu")
 
 SpikeLoader = DataLoader[Dataset[UInt8[th.Tensor, "Population 28 28"]]]
 
 if __name__ == "__main__":
+    th.no_grad()
     encode_data = partial(encode_data, num_steps=num_steps, population=population)
     data_train = MNIST(".", download=True, train=True, transform=encode_data)
     data_test = MNIST(".", download=True, train=False, transform=encode_data)
@@ -26,12 +34,26 @@ if __name__ == "__main__":
         data_test, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
 
-    net = Nessler2009(28 * 28 * population, out_features)
+    net = Nessler2009(28 * 28 * population, out_features, learning_rate).to(device)
+    wandb.watch(net)  # type: ignore
     try:
-        loader = iter(train_loader)
-        x = next(loader)[0].view(batch_size, num_steps, -1)
-        # x = data_train[0][0].view(1, num_steps, -1)
-        net(x)
-    except Exception as e:
+        for epoch in range(num_epochs):
+            for i, (data, target) in tqdm(enumerate(iter(train_loader))):
+                data = data.view(batch_size, num_steps, -1).to(device)
+                target = target.to(device)
+                net(data)
+                if i % 10 == 0:
+                    for k in range(out_features):
+                        evidence = net.prob_z2k.view(population, 28, 28, out_features)[
+                            ..., k
+                        ]
+                        evidence = 0.5 + 0.5 * (evidence[1] - evidence[0])
+                        wandb.log(
+                            {
+                                f"prob_z2k_{k}": wandb.Image(evidence),
+                                f"prob_z_{k}": net.prob_z[k],
+                            }
+                        )
+    except AssertionError as e:
         print(e)
         pdb.set_trace()
--- utils/Nessler2009.py
+++ utils/Nessler2009.py
@@ -1,19 +1,30 @@
+import pdb
 import torch as th
 from torch.nn import Module
-from jaxtyping import UInt8
+from jaxtyping import UInt8, Float
+from matplotlib import pyplot as plt
 
 
 class Nessler2009(Module):
-    last_input: UInt8[th.Tensor, "Batch Timesteps Population*28*28"]
-    # last_winners: UInt8[th.Tensor, "Batch Timesteps out_features"]
-    last_winner: UInt8[th.Tensor, "Batch Timesteps out_features"]
+    stdp_counter: int = 0
+    prob_z2k: Float[th.Tensor, "in_features out_features"]  # noqa: F722
+    prob_z: Float[th.Tensor, "out_features"]  # noqa: F821
 
-    def __init__(self, in_features: int, out_featuers: int = 10) -> None:
+    def __init__(
+        self, in_features: int, out_features: int = 10, learning_rate: float = 1e-3
+    ) -> None:
         super(Nessler2009, self).__init__()  # type: ignore
 
-        self.log_prob_z2k = -th.abs(th.rand((in_features, out_featuers)))
-        self.log_prob_z = -th.abs(th.rand((out_featuers)))
+        self.in_features = in_features
+        self.out_featuers = out_features
+        self.learning_rate = learning_rate
+        self.lr_decay_inverse = 1
         self.time_window = 10
+
+        prob_z2k = th.rand((in_features, out_features))
+        prob_z = th.rand((out_features))
+        self.register_buffer("prob_z2k", prob_z2k)
+        self.register_buffer("prob_z", prob_z)
 
     def forward(self, x: UInt8[th.Tensor, "Batch Timesteps Population*28*28"]) -> None:
         assert len(x.shape) == 3, (
@@ -24,28 +35,62 @@ class Nessler2009(Module):
         for t in range(1, self.time_window):
             x_windowed[:, t:, :] = x_windowed[:, t:, :] + x[:, :-t, :]
         x = x_windowed.sign_()
+        assert not x.isnan().any(), (
+            "NaN in x_windowed: {}".format(x),
+            pdb.set_trace(),
+        )
 
         for t in range(x.shape[1]):
-            spk_train = x[:, t, :]
-            self.last_input = x[:, t, :]
-            bayes_prob = spk_train.float() @ th.exp(self.log_prob_z2k) + th.exp(
-                self.log_prob_z
+            step_spike = x[:, t, :]
+            bayes_prob = (
+                th.exp(step_spike.float()) @ self.prob_z2k * self.prob_z
             )  # (Batch, out_features)
-            winners = bayes_prob.softmax(dim=1).multinomial(1)
-            # print(winners)
-            winners = th.nn.functional.one_hot(winners, 10)
-            winners = th.sum(winners, dim=0)
-            print(winners)
-            # self.last_winners[:, t, :] = th.nn.functional.one_hot(winner, 10)
-            self.STDP()
+            winners = bayes_prob.softmax(dim=1).multinomial(1).view(x.shape[0])
+            winners = th.nn.functional.one_hot(winners, self.out_featuers).to(
+                th.uint8
+            )  # (Batch, out_features)
+            self.STDP(step_spike, winners)
 
-    def STDP(self):
-        dw = (
-            th.zeros_like(self.log_prob_z2k)
-            .unsqueeze(0)
-            .repeat(self.last_input.shape[0], 1, 1)
-        )  # Calculate the weight change for each input of the batch
-        dw[
-            :,
-            :,
-        ]
+    def STDP(
+        self,
+        step_spike: UInt8[th.Tensor, "Batch in_features"],
+        winners: UInt8[th.Tensor, "Batch out_features"],
+    ) -> None:
+        assert step_spike.dtype == winners.dtype, (
+            f"{step_spike.dtype} != {winners.dtype}"
+        )
+        step_spike = step_spike.float()
+        winners = winners.float()
+        LTP_mask = step_spike.unsqueeze(-1) @ winners.unsqueeze(
+            1
+        )  # (Batch, in_features, out_features)
+        LTD_mask = (1 - step_spike).unsqueeze(-1) @ winners.unsqueeze(1)
+
+        dw = (self.prob_z2k - 1).unsqueeze_(0).repeat(
+            step_spike.shape[0], 1, 1
+        ) * LTP_mask - LTD_mask
+        self.prob_z2k += (
+            1
+            / self.lr_decay_inverse
+            * self.learning_rate
+            * dw.mean(dim=0)
+            * (self.prob_z2k)
+            * (1 - self.prob_z2k)
+        )  # mean over batch
+        self.prob_z2k = th.nn.functional.normalize(
+            self.prob_z2k, p=1, dim=0
+        )  # Normalize over in_features, to satisfy the constraint that the sum of the weights to each output neuron is 1 (the weights is prob of the input spike given the latent variable)
+
+        db = (self.prob_z - 1).unsqueeze_(0).repeat(
+            step_spike.shape[0], 1
+        ) * winners - (1 - winners)
+        self.prob_z += (
+            1
+            / self.lr_decay_inverse
+            * self.learning_rate
+            * db.mean(dim=0)
+            * (self.prob_z)
+            * (1 - self.prob_z)
+        )  # mean over batch
+        self.prob_z = th.nn.functional.normalize(self.prob_z, p=1, dim=0)
+        self.lr_decay_inverse += 1
